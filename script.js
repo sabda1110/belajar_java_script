a6c38b35cf6a1a82cec7013d18607ece2bc0ce8c
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-09 04:02:42 +0100

    Belajar memberikan popup

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
- var x =10;	
- console.log("Hello World");
- console.log("Isi dari nilai x adalah" + x);
+alert("Selamat Datang"); //hanya menampilkan popup
+var lagi = true;
+
+while(lagi==true)
+{
+	var nama =prompt("Masukan Nama : "); //memberikan inputan
+	alert("Nama : "+nama);
+	lagi = confirm("Lagi ? "); //memberikan nilai true atau false
+}
+alert("Terima Kasih");

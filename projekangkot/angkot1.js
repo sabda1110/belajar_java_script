671e20aacc8cba0828a4752152e8696be8a1444a
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-09 04:25:17 +0100

    Belajar Pengulangan

---
+++ projekangkot/angkot1.js
@@ -0,0 +1,6 @@
+var nilaiAwal=1; //nilai awal
+while(nilaiAwal <= 10) //suatu kondisi
+ { 
+ 	console.log("Angkot No . "+nilaiAwal+" Beroperasi dengan baik"); //aksi
+ nilaiAwal++; //increment(penambahan Nilai)
+  }

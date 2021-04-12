469970d897396f303dcaffec7e00849b7cbdfb6f
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-12 04:01:32 +0100

    Belajar array fungsi splice & slice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var nama =["Sabda","Bombom","Rahmad"];
+var nama =["Sabda","Bombom","Rahmad","ari","Fauzi"];
 
 // for(var i=0;i<nama.length;i++)
 // {
@@ -10,6 +10,10 @@ var nama =["Sabda","Bombom","Rahmad"];
 //nama.pop(); //menghapus nilai array yang terahkir
 //nama.unshift("ustad"); //memberikan nilai baru di depan array
 //nama.shift(); //menghapus nilai array yang di awal
+//namaarray.spliece(indexawal,elemet yang mau di hapus,element baru);
+//nama.splice(2,0,"Fahri");
 
+var baru =nama.slice(1,4);
 console.log(nama.join(" - "));
+console.log(baru.join(" - "));
 

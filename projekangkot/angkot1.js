c106a502cec748e8879299df205c46337a604c57
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-14 07:48:27 +0100

    Projek Menambahkan dan mengurangi pengguna satu angkot

--- projekangkot/angkot1.js
+++ projekangkot/angkot1.js
@@ -1,6 +1,57 @@
-var nilaiAwal=1; //nilai awal
-while(nilaiAwal <= 10) //suatu kondisi
- { 
- 	console.log("Angkot No . "+nilaiAwal+" Beroperasi dengan baik"); //aksi
- nilaiAwal++; //increment(penambahan Nilai)
-  }
+var penumpang=['Sabda',undefined,'Fahri'];
+
+var tambahpenumpang = function(namapenumpang,penumpang)
+{
+	if(penumpang.length == 0)
+	{
+      penumpang.push(namapenumpang);
+      return penumpang;
+	}
+
+	else
+	{
+		for(var i=0;i<penumpang.length;i++)
+		{
+			if(penumpang[i]==undefined)
+			{
+				penumpang[i]=namapenumpang;
+				return penumpang;
+			}
+			else if (penumpang[i]==namapenumpang)
+			{
+				console.log(namapenumpang +'Nama tersebut telah tersedia di angkot');
+				return penumpang;
+			}
+			else if (i == penumpang.length -1 )
+			{
+				penumpang.push(namapenumpang);
+				return penumpang;
+			}
+		}
+	}
+}
+
+var hapuspenumpang = function(namapenumpang,penumpang)
+{
+	if (penumpang.length ==0 )
+	{
+		console.log("Penumpang Kosong");
+		return penumpang;
+	}
+	else
+	{
+		for(var i=0;i<penumpang.length;i++)
+		{
+			if(namapenumpang == penumpang[i])
+			{
+				penumpang[i] = undefined;
+				return penumpang;
+			}
+			else if(i == penumpang.length -1)
+			{
+              console.log(namapenumpang + " Nama tersebut tidak tersedia");
+              return penumpang;
+			}
+		}
+	}
+}

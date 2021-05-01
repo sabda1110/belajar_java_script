706231718b7e63545c681d97c7116324491805ff
sabda1110 <sabdasetiawan205@gmail.com>
2021-05-01 12:40:36 +0100

    Belajar Object.Create

--- script.js
+++ script.js
@@ -11,38 +11,47 @@
 // }
 
 
-//2. Function Declarasi
+//2. OBject.create();
+let makanan = {
+  makan : function(porsi)
+  {
+    this.energi+=porsi;
+    console.log(`Hello ${this.nama},Selamat Makan Bro`);
+  },
+  tidur:function(jam){
+    this.energi-=jam;
+    console.log(`Helo ${this.nama},Selamat Bermain Bro`);
+  }
+}
 
-// function Mahasiswa(nama,energi){
-//   let mahasiswa={};
-//   mahasiswa.nama=nama;
-//   mahasiswa.energi=energi;
-//   mahasiswa.makan = function(porsi)
-//   {
-//     this.energi+=porsi;
-//     console.log(`Hello ${this.nama},Selamat Makan Bro`);
-//   }
-//   return mahasiswa;
-// }
 
-// Pemanggilan Objeck
-// let sabda = Mahasiswa('Sabda',10);
+function Mahasiswa(nama,energi){
+  let mahasiswa=Object.create(makanan);
+  mahasiswa.nama=nama;
+  mahasiswa.energi=energi;
+  // mahasiswa.makan=makanan.makan; //Jadinya Tidak Perlu Membuaat Seperti Ini Untuk object.create()
+  // mahasiswa.main=makanan.tidur; //Jadinya Tidak Perlu Membuaat Seperti Ini Untuk object.create()
+  return mahasiswa;
+}
+
+//Pemanggilan Objeck
+let sabda = Mahasiswa('Sabda',10);
 
 
 
 
 //3. Cunstuctor Function
 
-function Mahasiswa(nama,energi){
-  this.nama=nama;
-  this.energi=energi;
-  this.makan = function(porsi)
-  {
-    this.energi+=porsi;
-    console.log(`Hello ${this.nama},Selamat Makan Bro`);
-  }
+// function Mahasiswa(nama,energi){
+//   this.nama=nama;
+//   this.energi=energi;
+//   this.makan = function(porsi)
+//   {
+//     this.energi+=porsi;
+//     console.log(`Hello ${this.nama},Selamat Makan Bro`);
+//   }
   
-}
+// }
 
 // Pemanggilan Objeck
-let sabda = new Mahasiswa('Sabda',10);
+// let sabda = new Mahasiswa('Sabda',10);

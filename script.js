d5b95a022da1f820800c1a08c92b92017dd1a2bc
sabda1110 <sabdasetiawan205@gmail.com>
2021-05-04 14:39:55 +0100

    Belajar Prototype

--- script.js
+++ script.js
@@ -1,57 +1,41 @@
-// //1. Objek Literal
-
-// let mahasiswa = {
-//   nama : 'Sabda',
-//   energi : 10,
-//   makan:function(porsi)
-//   {
-//     this.energi += porsi;
-//     console.log(`Helo ${this.nama} , Selamat Makan`);
-//   }
+//Versi Prototype
+// function mahasiswa(nama,energi)
+// {
+//   //let Mahasiswa=object.create(mahasiswa.prototype) yang terjadi di balik layar
+//   this.nama=nama;
+//   this.energi=energi;
+// }
+// mahasiswa.prototype.makan = function(porsi)
+// {
+//   this.energi+=porsi;
+//   return `Helo ${this.nama},Selamat Makan`;
+// }
+// mahasiswa.prototype.main = function(jam)
+// {
+//   this.energi-=jam;
+//   return`Helo ${this.nama},Selamat Bermain`;
 // }
 
+// let sabda = new mahasiswa('Sabda',10);
 
-//2. OBject.create();
-let makanan = {
-  makan : function(porsi)
+
+//Versi Class
+class mahasiswa{
+  constructor(nama,energi)
+  {
+    this.nama=nama;
+    this.energi=energi;
+  }
+  makan(porsi)
   {
     this.energi+=porsi;
-    console.log(`Hello ${this.nama},Selamat Makan Bro`);
-  },
-  tidur:function(jam){
+    return`Helo ${this.nama},Selamat Makan`;
+  }
+  main(jam)
+  {
     this.energi-=jam;
-    console.log(`Helo ${this.nama},Selamat Bermain Bro`);
+    return`Helo ${this.nama},Selamat Bermain`;
   }
 }
 
-
-function Mahasiswa(nama,energi){
-  let mahasiswa=Object.create(makanan);
-  mahasiswa.nama=nama;
-  mahasiswa.energi=energi;
-  // mahasiswa.makan=makanan.makan; //Jadinya Tidak Perlu Membuaat Seperti Ini Untuk object.create()
-  // mahasiswa.main=makanan.tidur; //Jadinya Tidak Perlu Membuaat Seperti Ini Untuk object.create()
-  return mahasiswa;
-}
-
-//Pemanggilan Objeck
-let sabda = Mahasiswa('Sabda',10);
-
-
-
-
-//3. Cunstuctor Function
-
-// function Mahasiswa(nama,energi){
-//   this.nama=nama;
-//   this.energi=energi;
-//   this.makan = function(porsi)
-//   {
-//     this.energi+=porsi;
-//     console.log(`Hello ${this.nama},Selamat Makan Bro`);
-//   }
-  
-// }
-
-// Pemanggilan Objeck
-// let sabda = new Mahasiswa('Sabda',10);
+let siswa1 = new mahasiswa('sabda',10);

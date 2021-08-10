4ab0537ba93f69466dac194538b1579d28a6fa2e
sabda1110 <sabdasetiawan205@gmail.com>
2021-08-10 16:38:13 +0700

    Beljara Selection Part 2 :quertSelector & querySelectorAll

--- script.js
+++ script.js
@@ -18,6 +18,21 @@ const paragraf1 = document.getElementsByClassName('p1')[0];
 paragraf1.style.fontStyle='italic';
 
 
+//4. querySelector
+const p4 = document.querySelector('#b p');
+p4.style.color="red";
+
+const li = document.querySelector('#b ul li:nth-child(2)');
+li.style.backgroundColor="blue";
+
+//5. querySelectorAll()
+const paragraf4 = document.querySelectorAll('p');
+for(let i=0; i<paragraf4.length;i++){
+    if(i==3){
+        paragraf4[i].style.backgroundColor="green";
+    }
+}
+
 
 
 
